b5af871ce522013f73d7b3bfd27e444a4bc6dd70
KamilKowalczyk8 <61974258+KamilKowalczyk8@users.noreply.github.com>
2025-05-20 20:58:43 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:recipes/screens/tabs.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipes/data/database_helper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:recipes/screens/auth/login_screen.dart';
+import 'firebase_options.dart';
 
 final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
@@ -16,7 +20,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await DatabaseHelper.instance.database;
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: App()));
 }
 
@@ -25,6 +29,28 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const TabsScreen());
+    return MaterialApp(theme: theme, home: const AuthGate());
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          return const TabsScreen(); // użytkownik jest zalogowany
+        } else {
+          return const LoginScreen(); // użytkownik niezalogowany
+        }
+      },
+    );
   }
 }
